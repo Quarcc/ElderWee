27dde6606b49805fb7bb0b19c062e7470ce64bbd
Brandon <121778246+Quarcc@users.noreply.github.com>
2024-07-25 12:37:48 +0800

    25/07 - 1237

--- Frontend/src/component/admin/dmz/zone/dmzZone.jsx
+++ Frontend/src/component/admin/dmz/zone/dmzZone.jsx
@@ -38,12 +38,12 @@ const DMZZone = () => {
         }
 
         getFunds();
-    })
+    }, [])
 
     return(
         <div className="d-flex justify-content-center ms-auto me-auto mt-5">
-            <div className="col ms-5">
-                <h2>Demilitarized Zone</h2>
+            <div className="col ms-5 me-3 mainZoneBox">
+                <h2 className="ps-3 pt-3">Demilitarized Zone</h2>
                 <div className="dmz-container d-flex justify-content-center">
                     {transactions.map((transaction) => (
                         <div key={transaction.id} className="dmz-box">
@@ -54,18 +54,20 @@ const DMZZone = () => {
                     ))}
                 </div>
             </div>
-            <div className="col me-5">
-                <h2>Frozen Funds</h2>
-                <div className="dmz-container d-flex justify-content-center">   
-                {funds.map((transaction) => (
-                    transaction.TransactionStatus === "Pending" ? (
-                        <div key={transaction.TransactionID} className="dmz-box">
-                        <p>ID : {transaction.TransactionID}</p>
-                        <p>Amount : ${transaction.TransactionAmount}</p>
-                        <p>Status : {transaction.TransactionStatus}</p>
-                        </div>
-                    ) : null
-                    ))}
+            <div className="col me-5 ms-3 mainZoneBox">
+                <h2 className="ps-3 pt-3">Frozen Funds</h2>
+                <div className="outer-dmz-container">
+                    <div className="dmz-container d-flex justify-content-start">   
+                    {funds.map((transaction) => (
+                        transaction.TransactionStatus === "Pending" ? (
+                            <div key={transaction.TransactionID} className="dmz-box">
+                            <p>ID : {transaction.TransactionID}</p>
+                            <p>Amount : ${transaction.TransactionAmount}</p>
+                            <p>Status : {transaction.TransactionStatus}</p>
+                            </div>
+                        ) : null
+                        ))}
+                    </div>
                 </div>
             </div> 
         </div>
